4c2751060d3d76249e4bca6e5a854cdb39589ab4
Jonas <jonas.pommerening@gmail.com>
2014-01-31 10:55:01 +0100

    Switch to @Traumflug's mirror
    No more patches needed, maybe I'll get this to work some day

--- simulavr.rb
+++ simulavr.rb
@@ -2,16 +2,15 @@ require 'formula'
 
 class Simulavr < Formula
   homepage 'http://savannah.nongnu.org/projects/simulavr/'
-  url 'http://download.savannah.gnu.org/releases/simulavr/simulavr-1.0.0.tar.gz'
-  sha1 'e7cacc74be974793bd9c18330ec8d128fbd17d42'
+  url 'http://github.com/Traumflug/simulavr/tarball/c93f23872c85444b9bd0657755b9c674d180ae69'
+  sha1 '7757d5d3656c6260d3db48345cb7ebeade7e52ae'
 
   depends_on 'avr-binutils'
   depends_on 'swig-1.3'
 
   def patches
     patchdir = path.realpath.dirname.to_s
-    
-    [ patchdir + '/patches/simulavr-1.0.0-resize.patch' ]
+    []
   end
 
   def install
