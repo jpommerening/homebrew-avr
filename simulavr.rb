4f43cfb591732360d50ee8498bd83e2938d37e66
Jonas Pommerening <jonas.pommerening@gmail.com>
2014-05-28 01:57:23 +0200

    Uh, wrong way

--- simulavr.rb
+++ simulavr.rb
@@ -17,10 +17,10 @@ class Simulavr < Formula
   def patches
     patchdir = path.realpath.dirname.to_s
 
-    unless build.head?
-      [ 'file://' + patchdir + '/patches/simulavr-1.0.0-resize.patch' ]
+    if build.head?
+      [ 'file://' + patchdir + '/patches/simulavr-HEAD-glibtool.patch' ]
     else
-      [ 'file://' + patchdir + '/patches/simulavr-HEAD-glibtool.patch',
+      [ 'file://' + patchdir + '/patches/simulavr-1.0.0-resize.patch',
         'file://' + patchdir + '/patches/simulavr-1.0.0_avrgcc4.7.2-darwin.patch' ]
     end
   end
