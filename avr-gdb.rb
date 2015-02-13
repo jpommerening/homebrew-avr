0f5b2eba9d3ab39a0c8affd6b1a9ed8e488a64b9
Leo Soares <leojrfs@gmail.com>
2015-02-13 21:58:26 +0000

    Updated avr-gdb to 7.8.2

--- avr-gdb.rb
+++ avr-gdb.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class AvrGdb < Formula
   homepage 'http://www.gnu.org/software/gdb/'
-  version '7.8.1'
+  version '7.8.2'
   url "http://ftp.gnu.org/gnu/gdb/gdb-#{version}.tar.gz"
   mirror "http://ftpmirror.gnu.org/gnu/gdb/gdb-#{version}.tar.gz"
-  sha1 '36a9c4d365bf937f80eec13451bc6cc5eb94e562'
+  sha1 '67cfbc6efcff674aaac3af83d281cf9df0839ff9'
 
   depends_on 'avr-binutils'
 
